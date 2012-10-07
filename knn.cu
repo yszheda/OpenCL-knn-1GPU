cbb7b091c45bd03e30b4af9750ca99063c227fa8
Shuai YUAN <yszheda@gmail.com>
2012-10-07 23:34:07 +0800

    serial code using GPU

--- knn.cu
+++ knn.cu
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 
 #define INIT_MAX 100
+void showResult(int m, int k, int *out);
 
 /* 
 * m: total num of points
@@ -12,7 +13,7 @@
 * out: k nearest neighbors
 */
 
-void knn(int m, int n, int k, int *V, int *out)
+__global__ void knn(int m, int n, int k, int *V, int *out)
 {
 	int i,j;
 	int dim;
@@ -20,6 +21,9 @@ void knn(int m, int n, int k, int *V, int *out)
 	int sum;
 	int count;
 	int last_idx;
+//	int m = (*d_m);
+//	int n = (*d_n);
+//	int k = (*d_k);
 	for(i=0; i<m; i++)
 	{
 		temp = INIT_MAX;
@@ -45,14 +49,36 @@ void knn(int m, int n, int k, int *V, int *out)
 			}
 		}
 	}
+//	showResult(m, k, out);
 }
 
-int main(int argc, char *argv[])
+//__host__ __device__ void showResult(int m, int k, int *out)
+void showResult(int m, int k, int *out)
 {
-	int m,n,k;
 	int i,j;
-	int *V;
-	int *out;
+	for(i=0; i<m; i++)
+	{
+		for(j=0; j<k; j++)
+		{
+				printf("%d", out[i*k+j]);
+				if(j == k-1)
+				{
+						printf("\n");
+				}
+				else
+				{
+						printf(" ");
+				}
+		}
+	} 
+} 
+int main(int argc, char *argv[]) 
+{ 
+	int m,n,k;
+//	int *d_m, *d_n, *d_k;
+	int i,j;
+	int *V, *out;					//host copies
+	int *d_V, *d_out;			//device copies
 	FILE *fp;
 	if(argc != 2)
 	{
@@ -68,13 +94,31 @@ int main(int argc, char *argv[])
 	{
 		printf("m:%d, n:%d, k:%d\n", m, n, k);
 
+//		cudaMalloc((void **)&d_m, sizeof(int));
+//		cudaMalloc((void **)&d_n, sizeof(int));
+//		cudaMalloc((void **)&d_k, sizeof(int));
+//
+//		cudaMemcpy(d_m, &m, sizeof(int), cudaMemcpyHostToDevice);
+//		cudaMemcpy(d_n, &n, sizeof(int), cudaMemcpyHostToDevice);
+//		cudaMemcpy(d_k, &k, sizeof(int), cudaMemcpyHostToDevice);
+
 		V = (int *) malloc(m*n*sizeof(int));
 		out = (int *) malloc(m*k*sizeof(int));
+
+		cudaMalloc((void **)&d_V, m*n*sizeof(int));
+		cudaMalloc((void **)&d_out, m*k*sizeof(int));
+
 		for(i=0; i<m*n; i++)
 		{
 			fscanf(fp, "%d", &V[i]);
 		}
-		knn(m, n, k, V, out);
+		
+		cudaMemcpy(d_V, V, m*n*sizeof(int), cudaMemcpyHostToDevice);
+
+		knn<<<1,1>>>(m, n, k, d_V, d_out);
+
+		cudaMemcpy(out, d_out, m*k*sizeof(int), cudaMemcpyDeviceToHost);
+
 		for(i=0; i<m; i++)
 		{
 				for(j=0; j<k; j++)
@@ -90,8 +134,17 @@ int main(int argc, char *argv[])
 						}
 				}
 		}
+//		cudaFree(d_m);
+//		cudaFree(d_n);
+//		cudaFree(d_k);
+		cudaFree(d_V);
+		cudaFree(d_out);
+
 		free(V);
 		free(out);
+
+		V = NULL;
+		out = NULL;
 	}
 	fclose(fp);
 	return 0;
