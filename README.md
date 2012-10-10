fc1c7de695e713a87632623a3a89402347a757a3
Shuai YUAN <yszheda@gmail.com>
2012-10-10 20:33:51 +0800

    add comments to explain the design

---
+++ README.md
@@ -0,0 +1,11 @@
+# k-nearest neighbor using CUDA
+
+## Compile the program
+	$ make
+
+## Execute the program
+	$ chmod +x ./knn.o
+	$ ./knn.o <file-name>
+
+
+Shuai YUAN <yszheda@gmail.com>
--- knn.cu
+++ knn.cu
@@ -21,6 +21,8 @@ void showResult(int m, int k, int *out);
 
 extern __shared__ int block_dim_D[];
 
+// compute the square of distance per dimension
+// the kth dimension of the ith point and jth point
 __device__ void computeDimDist(int m, int n, int *V)
 {
 	int i = blockIdx.x;
@@ -29,6 +31,7 @@ __device__ void computeDimDist(int m, int n, int *V)
 	block_dim_D[j*n+k] = (V[i*n+k]-V[j*n+k])*(V[i*n+k]-V[j*n+k]);
 }
 
+// compute the square of distance of the ith point and jth point
 __device__ void computeDist(int m, int n, int *V, int *D)
 {
 	int i = blockIdx.x;
@@ -37,14 +40,16 @@ __device__ void computeDist(int m, int n, int *V, int *D)
 	int s;
 	int base_idx;
 	int dist = 0;
-	//dimention calculation
+	// calculate the square of distance per dimensions
 	computeDimDist(m, n, V);
 	__syncthreads();
-	//reduce duplications
 	base_idx = j*n;
+	// reduce duplicated calculations since d(i, j) = d(j, i)
+	// also, we do not consider the trivial case of d(i, i) = 0
+	// so we only compute the square distance when i < j 
 	if(i < j)
 	{
-		//paralle reduction
+		// use parallel reduction
 		for(s=n/2; s>0; s>>=1)
 		{
 			if(k < s)
@@ -54,7 +59,7 @@ __device__ void computeDist(int m, int n, int *V, int *D)
 		}
 		__syncthreads();
 		dist = block_dim_D[base_idx];
-		//when n is odd, the last element of dim_D needs to be added
+		// when n is odd, the last element of block_dim_D needs to be added
 		if(n > (n/2)*2)
 		{
 			dist += block_dim_D[base_idx+n-1];
@@ -63,6 +68,7 @@ __device__ void computeDist(int m, int n, int *V, int *D)
 	D[i*m+j] = dist;
 }
 
+// compute the k nearest neighbors
 __global__ void knn(int m, int n, int k, int *V, int *out, int *D)
 {
 	int i,j;
@@ -74,22 +80,24 @@ __global__ void knn(int m, int n, int k, int *V, int *out, int *D)
 
 	computeDist(m, n, V, D);
 	__syncthreads();
-	// let the first thread select the k-min dist
+	// find the k nearest neighbors of the point with index = blockIdx.x
 	i = blockIdx.x;
-	if(threadIdx.x == 0)
+	// let the first thread select the k-min distance
+	if(threadIdx.x == 0 && threadIdx.y == 0)
 	{
 		for(count=0; count<k; count++)
 		{
 			temp = INIT_MAX;
+			// iterate the jth point
 			for(j=0; j<m; j++)
 			{
+				// check whether the jth point is the same point as the ith one
+				// or has already in the k-nn list
 				is_duplicate = 0;
-
 				if(j == i)
 				{
 					is_duplicate = 1;
 				}
-				
 				for(num=0; num<count; num++)
 				{
 					if(out[i*k+num] == j)
@@ -97,7 +105,9 @@ __global__ void knn(int m, int n, int k, int *V, int *out, int *D)
 						is_duplicate = 1;
 					}
 				}
-	
+				// we have reduced duplicated calculation of the pair of d(i, j) and d(j, i) before,
+			    // and only one of them (here is the one that satisfies i<j) is valid
+				// now we need to load the correct one from the array D[]
 				if(!is_duplicate)
 				{
 					if(i < j)
@@ -144,10 +154,8 @@ int main(int argc, char *argv[])
 	int i;
 	int *V, *out;				//host copies
 	int *d_V, *d_out;			//device copies
-	
 	int *D;						
 //	int *dim_D;					//be replaced with shared memory
-
 	FILE *fp;
 	if(argc != 2)
 	{
@@ -161,14 +169,11 @@ int main(int argc, char *argv[])
 	}
 	while(fscanf(fp, "%d %d %d", &m, &n, &k) != EOF)
 	{
-		printf("m:%d, n:%d, k:%d\n", m, n, k);
-
 		V = (int *) malloc(m*n*sizeof(int));
 		out = (int *) malloc(m*k*sizeof(int));
-
+		// allocate space for devices copies
 		cudaMalloc((void **)&d_V, m*n*sizeof(int));
 		cudaMalloc((void **)&d_out, m*k*sizeof(int));
-
 		cudaMalloc((void **)&D, m*m*sizeof(int));
 //		cudaMalloc((void **)&dim_D, m*m*n*sizeof(int));
 
@@ -176,32 +181,33 @@ int main(int argc, char *argv[])
 		{
 			fscanf(fp, "%d", &V[i]);
 		}
-		
+		// copy host values to devices copies
 		cudaMemcpy(d_V, V, m*n*sizeof(int), cudaMemcpyHostToDevice);
 
-//		dim3 blk(m, m, n);
 		dim3 blk(m, n);
-
+		// compute the execution time
 		cudaEvent_t start, stop;
+		// create event
 		cudaEventCreate(&start);
 		cudaEventCreate(&stop);
+		// record event
 		cudaEventRecord(start);
-
+		// launch knn() kernel on GPU
 		knn<<<m, blk, m*n*sizeof(int)>>>(m, n, k, d_V, d_out, D);
-
+		// record event and synchronize
 		cudaEventRecord(stop);
 		cudaEventSynchronize(stop);
 		float time;
+		// get event elapsed time
 		cudaEventElapsedTime(&time, start, stop);
 		printf("GPU calculation time:%f ms\n", time);
-
+		// copy result back to host
 		cudaMemcpy(out, d_out, m*k*sizeof(int), cudaMemcpyDeviceToHost);
 
 		showResult(m, k, out);
-
+		// cleanup
 		cudaFree(d_V);
 		cudaFree(d_out);
-
 		cudaFree(D);
 //		cudaFree(dim_D);
 
