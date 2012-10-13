9c273774a5200a823d21f62c8290fd1be08686a6
Shuai YUAN <yszheda@gmail.com>
2012-10-13 11:50:02 +0800

    load data from GM to SM in a parallelized way, change output format

--- knn.cu
+++ knn.cu
@@ -16,17 +16,15 @@
 #include<cuda.h>
 #include<stdlib.h>
 
-#define INIT_MAX 100
+#define INIT_MAX 100000
 void showResult(int m, int k, int *out);
 
 extern __shared__ int SM[];
 
 // compute the square of distance per dimension
 // the kth dimension of the ith point and jth point
-__device__ void computeDimDist(int m, int n, int *V)
+__device__ void computeDimDist(int i, int j, int n, int *V)
 {
-	int i = blockIdx.x;
-   	int j = blockIdx.y;
 	int k = threadIdx.x;
 	SM[k] = (V[i*n+k]-V[j*n+k])*(V[i*n+k]-V[j*n+k]);
 }
@@ -38,15 +36,14 @@ __global__ void computeDist(int m, int n, int *V, int *D)
    	int j = blockIdx.y;
 	int k = threadIdx.x;
 	int s;
-	int dist = 0;
 	// calculate the square of distance per dimensions
-	computeDimDist(m, n, V);
-	__syncthreads();
 	// reduce duplicated calculations since d(i, j) = d(j, i)
 	// also, we do not consider the trivial case of d(i, i) = 0
 	// so we only compute the square distance when i < j 
-	if( i < j )
+	if(i < j)
 	{
+		computeDimDist(i, j, n, V);
+		__syncthreads();
 		// use paralel reduction
 		for(s=n/2; s>0; s>>=1)
 		{
@@ -54,32 +51,34 @@ __global__ void computeDist(int m, int n, int *V, int *D)
 			{
 				SM[k] += SM[k+s];
 			}
+			__syncthreads();
 		}
-		__syncthreads();
-		dist = SM[0];
-		// when n is odd, the last element of SM needs to be added
-		if(n > (n/2)*2)
+		if(k == 0)
 		{
-			dist += SM[n-1];
+			// when n is odd, the last element of SM needs to be added
+			if(n > (n/2)*2)
+			{
+				D[i*m+j] = SM[0] + SM[n-1];
+			}
+			else
+			{
+				D[i*m+j] = SM[0];
+			}
 		}
 	}
-	D[i*m+j] = dist;
 }
 
-__device__ void initSM(int *D)
+__device__ void initSM(int m, int *D)
 {
 	int i = blockIdx.x;
-	int j;
-	for(j=0; j<m; j++)
+	int j = threadIdx.x;
+	if(i < j)
 	{
-		if(i < j)
-		{
-			SM[j] = D[i*m+j];
-		}
-		else
-		{
-			SM[j] = D[j*m+i];	
-		}
+		SM[j] = D[i*m+j];
+	}
+	else
+	{
+		SM[j] = D[j*m+i];	
 	}
 	__syncthreads();
 }
@@ -96,8 +95,12 @@ __global__ void knn(int m, int n, int k, int *V, int *D, int *out)
 
 	// find the k nearest neighbors of the point with index = blockIdx.x
 	i = blockIdx.x;
+	
+	initSM(m, D);
+	__syncthreads();
+
 	// let the first thread select the k-min distance
-	if(threadIdx.x == 0 && threadIdx.y == 0)
+	if(threadIdx.x == 0)
 	{
 		for(count=0; count<k; count++)
 		{
@@ -119,19 +122,9 @@ __global__ void knn(int m, int n, int k, int *V, int *D, int *out)
 						is_duplicate = 1;
 					}
 				}
-				// we have reduced duplicated calculation of the pair of d(i, j) and d(j, i) before,
-			    // and only one of them (here is the one that satisfies i<j) is valid
-				// now we need to load the correct one from the array D[]
 				if(!is_duplicate)
 				{
-					if(i < j)
-					{
-						dist = D[i*m+j];
-					}
-					else
-					{
-						dist = D[j*m+i];
-					}
+					dist = SM[j];
 					if(dist < temp)
 					{
 						temp = dist;
@@ -150,15 +143,17 @@ void showResult(int m, int k, int *out)
 	{
 		for(j=0; j<k; j++)
 		{
-			printf("%d", out[i*k+j]);
+			printf("%d ", out[i*k+j]);
 			if(j == k-1)
 			{
 				printf("\n");
 			}
+			/*
 			else
 			{
 				printf(" ");
 			}
+			*/
 		}
 	} 
 } 
@@ -169,19 +164,24 @@ int main(int argc, char *argv[])
 	int *V, *out;				//host copies
 	int *d_V, *d_out;			//device copies
 	int *D;						
-//	int *dim_D;					//be replaced with shared memory
-	FILE *fp;
+	FILE *fp_in;
+	FILE *fp_out;
 	if(argc != 2)
 	{
-		printf("Usage: knn [file path]\n");
+		printf("Usage: knn inputfile\n");
 		exit(1);
 	}
-	if((fp = fopen(argv[1], "r")) == NULL)
+	if((fp_in = fopen(argv[1], "r")) == NULL)
 	{
-		printf("Error open file!\n");
+		printf("Error open input file!\n");
 		exit(1);
 	}
-	while(fscanf(fp, "%d %d %d", &m, &n, &k) != EOF)
+	if((fp_out = fopen("time.txt", "w")) == NULL)
+	{
+		printf("Error open output file!\n");
+		exit(1);
+	}
+	while(fscanf(fp_in, "%d %d %d", &m, &n, &k) != EOF)
 	{
 		V = (int *) malloc(m*n*sizeof(int));
 		out = (int *) malloc(m*k*sizeof(int));
@@ -189,16 +189,14 @@ int main(int argc, char *argv[])
 		cudaMalloc((void **)&d_V, m*n*sizeof(int));
 		cudaMalloc((void **)&d_out, m*k*sizeof(int));
 		cudaMalloc((void **)&D, m*m*sizeof(int));
-//		cudaMalloc((void **)&dim_D, m*m*n*sizeof(int));
 
 		for(i=0; i<m*n; i++)
 		{
-			fscanf(fp, "%d", &V[i]);
+			fscanf(fp_in, "%d", &V[i]);
 		}
 		// copy host values to devices copies
 		cudaMemcpy(d_V, V, m*n*sizeof(int), cudaMemcpyHostToDevice);
 
-		dim3 blk(m, n);
 		dim3 grid(m, m);
 		// compute the execution time
 		cudaEvent_t start, stop;
@@ -217,7 +215,7 @@ int main(int argc, char *argv[])
 		float time;
 		// get event elapsed time
 		cudaEventElapsedTime(&time, start, stop);
-		printf("GPU calculation time:%f ms\n", time);
+		fprintf(fp_out, "GPU calculation time:%f ms\n", time);
 		// copy result back to host
 		cudaMemcpy(out, d_out, m*k*sizeof(int), cudaMemcpyDeviceToHost);
 
@@ -226,12 +224,12 @@ int main(int argc, char *argv[])
 		cudaFree(d_V);
 		cudaFree(d_out);
 		cudaFree(D);
-//		cudaFree(dim_D);
 
 		free(V);
 		free(out);
 	}
-	fclose(fp);
+	fclose(fp_in);
+	fclose(fp_out);
 	return 0;
 }
 
